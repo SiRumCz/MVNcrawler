c1bf051eb2d84b9b2111963eb4e46034abfaeba1
zeyang919 <920193796@qq.com>
2021-10-01 13:09:11 -0400

    get dependencies using groupID and artifactID

--- 01.py
+++ 01.py
@@ -1,14 +1,14 @@
 from getWeb import getWebContent
-from getVersionDetail import getLastVersionDetail
+from getVersionDetail import getLastVersionDetail, getComDep
 from getVersionDetail import getVersionDetail, getGroupId
 
-mvn_url = "https://mvnrepository.com/artifact/"
+mvn_url = "https://mvnrepository.com/artifact"
 groupId = "edu.uci.ics"
 artifactId = "crawler4j"
-url = mvn_url + "/" + groupId + "/" + artifactId
+url = mvn_url + "/" + groupId + "/" + artifactId + "/"
 
 # content = getWebContent("https://mvnrepository.com/artifact/com.dimafeng/testcontainers-scala-localstack")
-content = getWebContent(mvn_url + groupId + "/" + artifactId)
+content = getWebContent(mvn_url + "/" + groupId + "/" + artifactId + "/")
 
 print(type(content))
 # print(getGroupId(content))
@@ -20,3 +20,5 @@ print(getVersionDetail(content))
 #     print(it.group("href"))
 #     print(it.group("version"))
 #     print(it.group("usages"))
+
+print(getComDep(getWebContent("https://mvnrepository.com/artifact/edu.uci.ics/crawler4j/4.4.0")))
--- getVersionDetail.py
+++ getVersionDetail.py
@@ -1,5 +1,7 @@
 import re
 
+from getWeb import getWebContent
+
 
 def getLastVersionDetail(content):
     '''
@@ -43,3 +45,59 @@ def getGroupId(content):
     groupOfLib = obj.findall(content)
 
     return groupOfLib[0]
+
+
+def getComDep(content):
+    obj1 = re.compile(r'(?P<main><h2>Compile Dependencies.*?</table>)', re.S)
+    if len(obj1.findall(content)) == 0:
+        return []
+    main_content = obj1.findall(content)[0]
+    obj = re.compile(
+        r'<tr>.*?</td>.*?</td><td>.*?>(?P<groupId>.*?)</a>[<\n]»\n.*?>(?P<artifactId>.*?)</a></td>.*?<a.*?>\n(?P<version>.*?)</a>.*?</tr>',
+        re.S)
+
+    compileDep = obj.findall(main_content)
+
+    return compileDep
+
+
+def getTestDep(content):
+    obj1 = re.compile(r'<h2>Test Dependencies(?P<main>.*?)</table>', re.S)
+    if len(obj1.findall(content)) == 0:
+        return []
+    main_content = obj1.findall(content)[0]
+    obj = re.compile(
+        r'<tr>.*?</td>.*?</td><td>.*?>(?P<groupId>.*?)</a>[<\n]»\n.*?>(?P<artifactId>.*?)</a></td>.*?<a.*?>\n(?P<version>.*?)</a>.*?</tr>',
+        re.S)
+
+    testDep = obj.findall(main_content)
+
+    return testDep
+
+
+def getRunDep(content):
+    obj1 = re.compile(r'<h2>Runtime Dependencies(?P<main>.*?)</table>', re.S)
+    if len(obj1.findall(content)) == 0:
+        return []
+    main_content = obj1.findall(content)[0]
+    obj = re.compile(
+        r'<tr>.*?</td>.*?</td><td>.*?>(?P<groupId>.*?)</a>[<\n]»\n.*?>(?P<artifactId>.*?)</a></td>.*?<a.*?>\n(?P<version>.*?)</a>.*?</tr>',
+        re.S)
+
+    runDep = obj.findall(main_content)
+
+    return runDep
+
+
+mvn_url = "https://mvnrepository.com/artifact"
+groupId = "edu.uci.ics"
+artifactId = "crawler4j"
+versionList = []
+url = mvn_url + "/" + groupId + "/" + artifactId + "/"
+versionList = getVersionDetail(getWebContent(url))
+print(versionList)
+for i in versionList:
+    print(i[0])
+    print(getComDep(getWebContent(url + i[1])))
+    print(getRunDep(getWebContent(url + i[1])))
+    print(getTestDep(getWebContent(url + i[1])))
